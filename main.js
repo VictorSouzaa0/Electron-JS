3d97c5de0107ce94e275abb30bcd9959175c6b70
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2026-06-11 22:07:23 -0300

    fix: window is opening

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 const {app, BrowserWindow} = require('electron')
 
-app.whenReady().then(() =>{
-    //create window
+
+const createWindow = () => {
+    
     const myWindow = new BrowserWindow({
         width: 1280,
         height: 720,
         webPreferences: {
             nodeIntegration: true
         }
-    });
-
-    // loading html page
-    myWindow.loadFile('index.html');
-    
-    // loading console
-    myWindow.webContents.openDevTools();
+    })
+        // loading html page    
+        myWindow.loadFile('index.html');
+        
+        // loading console
+        myWindow.webContents.openDevTools();
+}
+app.whenReady().then(() => {
+    createWindow();
 })
